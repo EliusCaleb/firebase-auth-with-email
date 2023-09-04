1963fc7e9b112f1ba04c95de914ee97b002d6d1d
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-09-04 17:19:52 +0300

    used a public template for this components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Signup from "./components/Signup";
+import { Link } from "react-router-dom";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={"/sign-in"}>
+              FIREBASE
+            </Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/sign-in"}>
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/sign-up"}>
+                    Sign up
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/sign-in" element={<Login />} />
+              <Route path="/sign-up" element={<Signup />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
